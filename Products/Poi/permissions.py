29eac4a01b55eb02d66871282e8ce725a0a8bc57
Cesar Augusto <cesaraugusto@liberiun.com>
2012-03-23 14:42:09 -0300

    Adição do produto costumizado com portlet e tradução

---
+++ Products/Poi/permissions.py
@@ -0,0 +1,17 @@
+# Permissions used by Poi
+
+from Products.CMFCore import permissions as CMFCorePermissions
+
+AccessContentsInformation = CMFCorePermissions.AccessContentsInformation
+ModifyPortalContent = CMFCorePermissions.ModifyPortalContent
+View = CMFCorePermissions.View
+
+EditResponse = "Poi: Edit response"
+ManageTracker = "Poi: Manage tracker"
+ModifyIssueAssignment = "Poi: Modify issue assignment"
+ModifyIssueSeverity = "Poi: Modify issue severity"
+ModifyIssueState = "Poi: Modify issue state"
+ModifyIssueTags = "Poi: Modify issue tags"
+ModifyIssueTargetRelease = "Poi: Modify issue target release"
+ModifyIssueWatchers = "Poi: Modify issue watchers"
+UploadAttachment = "Poi: Upload attachment"
---
+++ Products/Poi/tests/test_browser.py
@@ -0,0 +1,19 @@
+import unittest
+
+from zope.testing import doctest
+from Testing.ZopeTestCase import FunctionalDocFileSuite as Suite
+
+from Products.Poi.tests.ptc import PoiFunctionalTestCase
+
+OPTIONFLAGS = (doctest.ELLIPSIS |
+               doctest.NORMALIZE_WHITESPACE)
+
+
+def test_suite():
+    suites = [
+        Suite('browser.txt',
+              package='Products.Poi.tests',
+              optionflags=OPTIONFLAGS,
+              test_class=PoiFunctionalTestCase),
+        ]
+    return unittest.TestSuite(suites)
